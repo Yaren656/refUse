ec232b265a36b96a341dd03ea9a7c0f89878f1c2
Yaren Yuvka <yarenyuvka@gmail.com>
2021-01-17 21:43:29 +0300

    Add project files.

---
+++ refUse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace refUse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 50;
+            var result = Divide(number1, number2);
+
+            Console.WriteLine("Sonuç : " + result);
+            //Console.WriteLine("Main'deki number1 : " + number1);
+            Console.ReadLine();
+        }
+
+        static int Divide(int number1, int number2)
+        {
+            number1 = 5;  // yazmak zorunda değiliz fakat değer değişimi görmek için yazdım.
+            //Console.WriteLine("Method'daki number1 : " + number1);
+            return number2 / number1;
+        }
+    }
+}
